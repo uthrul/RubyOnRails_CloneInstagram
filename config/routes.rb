454616b141b3cdafa038fcdb0d3365ab39dc0173
saud maruli <uthrul@gmail.com>
2018-08-24 13:04:00 +0700

    clone web instagram layout

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  root 'posts#index'
+
+  devise_for :users,
+    path:'',
+    path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
+    controllers: {omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations'}
+
+  # get 'user/:id', to: 'user#show'
+  # /users/3 -> Users controller, show action, params (id: '3')
+  resources :users, only: [:show]
+
+    resources :posts, only: [:index, :show, :create, :destroy] do
+      resources :photos, only: [:create]
+    end
+end
